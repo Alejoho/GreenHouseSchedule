0cb743de063445020c1aff171f8cdc8c9964fc29
alejoho37 <alejoho37@gmail.com>
2023-08-09 12:01:30 -0400

    Included the DB in the projects, changed the connString and updated the entities

--- DataAccess/SowScheduleEntity.Designer.cs
+++ DataAccess/SowScheduleEntity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Alejandro\Documents\GH\GHSchedule\DataAccess\SowScheduleEntity.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Alejandro\Documents\GH\GreenHouseSchedule\DataAccess\SowScheduleEntity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
