84d164e7b40b2a1a4a2597c70dc933d15955455e
Alejoho <alejoreilly96@gmail.com>
2023-10-26 08:34:35 -0400

    Made some part of the GreenHouse window. But I change to make the other addeddit.

--- Presentation/Forms/GreenHousesWindow.xaml.cs
+++ Presentation/Forms/GreenHousesWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SupportLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,49 @@ namespace Presentation.Forms
     /// </summary>
     public partial class GreenHousesWindow : Window
     {
+        List<GreenHouse> _greenHouses;
         public GreenHousesWindow()
         {
             InitializeComponent();
+            _greenHouses = new List<GreenHouse>();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            _greenHouses.Add(new GreenHouse()
+            {
+                Id = 1,
+                Name = "casa 1",
+                Description = "es una casa grande",
+                Width = 12,
+                Length = 35,
+                GreenHouseArea = 420,
+                SeedTrayArea = 330,
+                AmountOfBlocks = 4,
+                Active = true
+            });
+
+            _greenHouses.Add(new GreenHouse()
+            {
+                Id = 2,
+                Name = "casa 2",
+                Description = "es una casa pequena",
+                Width = 6,
+                Length = 20,
+                GreenHouseArea = 120,
+                SeedTrayArea = 80,
+                AmountOfBlocks = 2,
+                Active = false
+            });
+
+            dgGreenHouses.ItemsSource = _greenHouses;
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
 
         //NEXT - Do all the logic for showing all the houses, edit them and delete them
