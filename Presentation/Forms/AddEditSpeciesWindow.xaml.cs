c9dbce4c95d6ac52aa92ba1d44a63bd1d1996083
Alejoho <alejoreilly96@gmail.com>
2023-11-08 20:27:29 -0500

    Almost ready the AddEditSpecies window

--- Presentation/Forms/AddEditSpeciesWindow.xaml.cs
+++ Presentation/Forms/AddEditSpeciesWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Processors;
+using SupportLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,11 +21,59 @@ namespace Presentation.Forms
     /// </summary>
     public partial class AddEditSpeciesWindow : Window
     {
+        private SpeciesProcessor _processor;
+        private Species _model;
+
         public AddEditSpeciesWindow()
         {
             InitializeComponent();
+            _processor = new SpeciesProcessor();
+            _model = new Species();
+            _model.Id = 0;
         }
 
-        //NEXT - do the logic for the addeditspecies window
+        public AddEditSpeciesWindow(Species model)
+        {
+            InitializeComponent();
+            _processor = new SpeciesProcessor();
+            this._model = model;
+            PopulateData();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if(ValidateDataType() == true)
+            {
+                if(_processor.SaveSpecies(_model)==true) 
+                {
+                    MessageBox.Show("Registro salvado");
+                    this.Close();
+                }
+                else
+                {
+                    ShowError();
+                }
+            }
+        }
+
+        private void ShowError()
+        {
+            MessageBox.Show(_processor.Error);
+        }
+        //NEXT - do the ValidateDataType method
+        private bool ValidateDataType()
+        {
+            throw new NotImplementedException();
+        }
+        //NEXT - do the PopulateData method
+        private void PopulateData()
+        {
+
+        }
     }
 }
