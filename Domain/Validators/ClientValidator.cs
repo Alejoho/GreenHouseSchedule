f4c18bd27c0668ec8395510a88c51e6cbe8d0e6f
Alejoho <alejoreilly96@gmail.com>
2023-11-27 08:08:57 -0500

    Ready the ClientValidator

---
+++ Domain/Validators/ClientValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using SupportLayer.Models;
+
+namespace Domain.Validators;
+
+public class ClientValidator : AbstractValidator<Client>
+{
+    public ClientValidator()
+	{
+        RuleLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(x => x.Name).NotEmpty().WithName("Nombre")
+            .WithMessage("El {PropertyName} no debe estar vacío.")
+            .MaximumLength(50)
+            .WithMessage("El {PropertyName} no debe exceder los 50 caracteres.");
+        RuleFor(x => x.NickName).MaximumLength(50).When(x => x.NickName != "")
+            .WithName("Apodo")
+            .WithMessage("El {PropertyName} no debe exceder los 50 caracteres.");
+        //TODO - use a regular expresion to validate the numbers
+        RuleFor(x => x.PhoneNumber).MaximumLength(20).When(x => x.PhoneNumber != "")
+            .WithName("Celular")
+            .WithMessage("El {PropertyName} no debe exceder los 20 caracteres.");
+        RuleFor(x => x.OtherNumber).MaximumLength(20).When(x => x.OtherNumber != "")
+            .WithName("Teléfono")
+            .WithMessage("El {PropertyName} no debe exceder los 20 caracteres.");
+        RuleFor(x => x.OrganizationId).GreaterThan((short)0).WithName("Organización")
+            .WithMessage("La {PropertyName} no debe estar vacía.");
+    }
+}
