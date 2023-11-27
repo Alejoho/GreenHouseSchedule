66d7d08e488ac26e7a08ea32fb318be7562586c9
Alejoho <alejoreilly96@gmail.com>
2023-11-27 07:38:08 -0500

    Ready the binding of the clients datagrid

--- Presentation/Forms/ClientsWindow.xaml.cs
+++ Presentation/Forms/ClientsWindow.xaml.cs
@@ -1,42 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-using System.Runtime.InteropServices;
-using System.Reflection.Metadata;
+namespace Presentation.Forms;
 
-namespace Presentation.Forms
+/// <summary>
+/// Interaction logic for Clients.xaml
+/// </summary>
+/// 
+public partial class ClientsWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for Clients.xaml
-    /// </summary>
-    /// 
-    public partial class ClientsWindow : Window
+    //NEXT - do the logic of the Clients window
+    public ClientsWindow()
     {
-        public ClientsWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        
+    }
 
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            e.Cancel= true;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            App.Current.Shutdown();
-            //this.Close();
-        }
+    private void dgClients_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        throw new NotImplementedException();
     }
 }
---
+++ SupportLayer/ViewModels/ClientView.cs
@@ -0,0 +1,9 @@
+﻿namespace SupportLayer.Models;
+
+public partial class Client
+{
+    public string TypeAndOrganizationName
+    {
+        get { return $"{Organization.TypeOfOrganization.Name} - {Organization.Name}"; }
+    }
+}
