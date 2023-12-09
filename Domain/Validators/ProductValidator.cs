c45ca3ec48f40375f66e823bdd64c514e5e24536
Alejoho <alejoreilly96@gmail.com>
2023-12-09 07:23:12 -0500

    Created the product validator

---
+++ Domain/Validators/ProductValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using SupportLayer.Models;
+
+namespace Domain.Validators;
+
+public class ProductValidator : AbstractValidator<Product>
+{
+    public ProductValidator()
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(x => x.SpecieId).GreaterThan((byte)0).WithName("Species")
+            .WithMessage("La {PropertyName} no debe estar vacía.");
+
+        RuleFor(x => x.Variety).NotEmpty().WithName("Nombre")
+            .WithMessage("El {PropertyName} no debe estar vacío.")
+            .MaximumLength(50)
+            .WithMessage("El {PropertyName} no debe exceder los 50 caracteres.");
+    }
+}
