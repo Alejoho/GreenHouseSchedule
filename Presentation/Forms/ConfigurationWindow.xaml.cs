eaf1e183592bc11bf77abde2118ca1c1675d0efe
Alejoho <alejoreilly96@gmail.com>
2023-12-12 10:56:15 -0500

    Ready the logic to show the configurations; and the button to show the Organization windows, in the Configuration window.

--- Presentation/Forms/ConfigurationWindow.xaml.cs
+++ Presentation/Forms/ConfigurationWindow.xaml.cs
@@ -1,27 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SupportLayer;
+using System.Configuration;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace Presentation.Forms
+namespace Presentation.Forms;
+
+/// <summary>
+/// Interaction logic for ConfigurationWindow.xaml
+/// </summary>
+public partial class ConfigurationWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for ConfigurationWindow.xaml
-    /// </summary>
-    public partial class ConfigurationWindow : Window
+    public ConfigurationWindow()
     {
-        public ConfigurationWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        LoadData();
+    }
+
+    private void LoadData()
+    {
+        lbltxtRegressionDays.FieldContent = ConfigurationManager
+            .AppSettings[ConfigurationNames.RegressionDays] ?? "null";
+        lbltxtDailySowingPotential.FieldContent = ConfigurationManager
+            .AppSettings[ConfigurationNames.DailySowingPotential] ?? "null";
+        lbltxtMinimumLimitOfSowPerDay.FieldContent = ConfigurationManager
+            .AppSettings[ConfigurationNames.MinimumLimitOfSowPerDay] ?? "null";
+        lbltxtLocationMinimumSeedTray.FieldContent = ConfigurationManager
+            .AppSettings[ConfigurationNames.LocationMinimumSeedTray] ?? "null";
+        lbltxtSeedlingMultiplier.FieldContent = ConfigurationManager
+            .AppSettings[ConfigurationNames.SeedlingMultiplier] ?? "null";
+        lbltxtSowShowRange.FieldContent = ConfigurationManager
+            .AppSettings[ConfigurationNames.SowShowRange] ?? "null";
+        lbltxtDeliveryShowRange.FieldContent = ConfigurationManager
+            .AppSettings[ConfigurationNames.DeliveryShowRange] ?? "null";
+    }
+
+    //LATER - In general in the windows with datagrids there is a button named "atras"
+    //but in code it's named btnCanel. Changed its name to btnBack.
+    private void btnBack_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+
+    private void btnOrganization_Click(object sender, RoutedEventArgs e)
+    {
+        OrganizationsWindow window = new OrganizationsWindow();
+        window.ShowDialog();
+    }
+
+    private void btnSave_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
