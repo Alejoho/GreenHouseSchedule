8698861ee3093d484f5f26107f1833b149f8106c
Alejoho <alejoreilly96@gmail.com>
2023-12-16 21:10:38 -0500

    created a class to hold the configurations properties

---
+++ Domain/Configurations.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain
+{
+    public class Configurations
+    {
+        public int RegressionDays { get; set; }
+        public int DailySowingPotential { get; set; }
+        public int MinimumLimitOfSowPerDay { get; set; }
+        public int LocationMinimumSeedTray { get; set; }
+        public double SeedlingMultiplier { get; set; }
+        public int SowShowRange { get; set; }
+        public int DeliveryShowRange { get; set; }
+    }
+}
