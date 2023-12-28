cf83dbebc48907b8aa0a9e343f52698272b4665e
Alejoho <alejoreilly96@gmail.com>
2023-12-28 10:39:13 -0500

    Created all Product window but the logic to add a new specie in window

--- Presentation/Forms/AddProductWindow.xaml.cs
+++ Presentation/Forms/AddProductWindow.xaml.cs
@@ -1,37 +1,86 @@
-﻿using System;
+﻿using Domain.Processors;
+using SupportLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace Presentation.Forms
+namespace Presentation.Forms;
+
+/// <summary>
+/// Interaction logic for AddProductWindow.xaml
+/// </summary>
+public partial class AddProductWindow : Window, ISpeciesRequester
 {
-    /// <summary>
-    /// Interaction logic for AddProductWindow.xaml
-    /// </summary>
-    public partial class AddProductWindow : Window
+    private ProductProcessor _processor;
+    private Product _model;
+    private List<Species> _species;
+    IProductRequester _requester;
+
+    public AddProductWindow(IProductRequester requestingWindow)
     {
-        public AddProductWindow()
+        InitializeComponent();
+        _processor = new ProductProcessor();
+        _model = new Product();
+        _requester = requestingWindow;
+        LoadData();        
+    }
+
+    private void LoadData()
+    {
+        SpeciesProcessor speciesProcessor = new SpeciesProcessor();
+        _species = speciesProcessor.GetAllSpecies().ToList();
+        lblcmbbtnSpecies.ComboBox.ItemsSource = _species;
+        lblcmbbtnSpecies.ComboBox.DisplayMemberPath = "Name";
+    }
+
+    private void btnSave_Click(object sender, RoutedEventArgs e)
+    {
+        if (ValidateDataType() == true)
         {
-            InitializeComponent();
+            if (_processor.SaveProduct(_model) == true)
+            {
+                MessageBox.Show("Registro salvado");
+                _requester?.ProductComplete(_model);
+                this.Close();
+            }
+            else
+            {
+                ShowError();
+            }
+        }
+    }
+
+    private void ShowError()
+    {
+        MessageBox.Show(_processor.Error);
+    }
+
+    private bool ValidateDataType()
+    {
+        bool output = true;
+        _model.Variety = lbltxtVariety.FieldContent;
+
+        if (lblcmbbtnSpecies.ComboBox.SelectedItem != null)
+        {
+            _model.SpecieId = ((Species)lblcmbbtnSpecies.ComboBox.SelectedItem).Id;
         }
 
-        private void btnSave_Click(object sender, RoutedEventArgs e)
-        {
+        return output;
+    }
 
-        }
+    private void btnCancel_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
 
-        private void btnCancel_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
+    private void lblcmbbtnSpecies_ButtonClick(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    public void SpeciesComplete(Species model)
+    {
+        throw new System.NotImplementedException();
     }
 }
