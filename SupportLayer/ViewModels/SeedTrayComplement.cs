20ac8b5a772ea4117082d68aa95fc23ffb10c4da
Alejoho <alejoreilly96@gmail.com>
2024-01-01 09:33:32 -0500

    Found a way to achive the bind between the checbox and IsSelsected property of a seedtray

--- SupportLayer/ViewModels/SeedTrayComplement.cs
+++ SupportLayer/ViewModels/SeedTrayComplement.cs
@@ -1,9 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
 
 namespace SupportLayer.Models;
 
-public partial class SeedTray
+public partial class SeedTray : INotifyPropertyChanged
 {
+
+    private bool isSelected = false;
+
     [NotMapped]
-    public bool IsSelected { get; set; } = false;
+    public bool IsSelected
+    {
+        get { return isSelected; }
+
+        set
+        {
+            if(value != IsSelected)
+            {
+                isSelected = value;
+                NotifyPropertyChanged();
+            }   
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
