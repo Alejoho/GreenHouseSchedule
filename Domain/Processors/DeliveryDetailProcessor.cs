18a2a4f058c175c29c074b366bb5ef85a5bf522f
Alejoho <alejoreilly96@gmail.com>
2024-01-03 21:59:12 -0500

    Created the DeliveryDetail processor with just the needed methods

---
+++ Domain/Processors/DeliveryDetailProcessor.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Repositories;
+using SupportLayer.Models;
+
+namespace Domain.Processors;
+
+public class DeliveryDetailProcessor
+{
+    private DeliveryDetailRepository _repository;
+
+    public DeliveryDetailProcessor()
+    {
+        _repository = new DeliveryDetailRepository();
+    }
+
+    public IEnumerable<DeliveryDetail> GetDeliveryDetailFromADateOn(DateOnly date)
+    {
+        IEnumerable<DeliveryDetail> output = _repository.GetByADeliveryDateOn(date)
+            .OrderBy(x => x.DeliveryDate);
+
+        return output;
+    }
+}
