da3ff8fff246ab87782ff638212128d001be6342
Alejoho <alejoreilly96@gmail.com>
2024-01-03 21:36:00 -0500

    Created the OrderLocation processor with just the necessary methods

---
+++ Domain/Processors/OrderLocationProcessor.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Repositories;
+using SupportLayer.Models;
+
+namespace Domain.Processors;
+
+public class OrderLocationProcessor
+{
+    private OrderLocationRepository _repository;
+
+    public OrderLocationProcessor()
+    {
+        _repository = new OrderLocationRepository();
+    }
+
+    public IEnumerable<OrderLocation> GetOrderLocationsFromADateOn(DateOnly date)
+    {
+        IEnumerable<OrderLocation> output = _repository.GetByASowDateOn(date)
+            .OrderBy(x => x.Id)
+            .ThenBy(x => x.SowDate);
+
+        return output;
+    }
+}
