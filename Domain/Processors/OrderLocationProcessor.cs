8552bf2bc32f8a1cbe7cdee6dae426b32cb897e4
Alejoho <alejoreilly96@gmail.com>
2024-04-13 09:34:09 -0400

    Exchange the ordenation order for the order locations

--- Domain/Processors/OrderLocationProcessor.cs
+++ Domain/Processors/OrderLocationProcessor.cs
@@ -14,10 +14,9 @@ public class OrderLocationProcessor : IOrderLocationProcessor
 
     public IEnumerable<OrderLocation> GetOrderLocationsFromADateOn(DateOnly date)
     {
-        //LATER - maybe exchange the order of ordenation.
         IEnumerable<OrderLocation> output = _repository.GetByASowDateOn(date)
-            .OrderBy(x => x.Id)
-            .ThenBy(x => x.SowDate);
+            .OrderBy(x => x.SowDate)
+            .ThenBy(x => x.Id);
 
         return output;
     }
