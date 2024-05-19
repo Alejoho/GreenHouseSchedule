1bda3f6d0d36c6223c2daad0700655c022212f77
Alejoho <alejoreilly96@gmail.com>
2024-05-19 07:28:32 -0400

    Added the "NotMapped" attribute to the new property of the SeedTray class

--- SupportLayer/ViewModels/OrderLocation.cs
+++ SupportLayer/ViewModels/OrderLocation.cs
@@ -1,6 +1,9 @@
-﻿namespace SupportLayer.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupportLayer.Models;
 
 public partial class OrderLocation
 {
+    [NotMapped]
     public string SeedTrayName { get; set; }
 }
