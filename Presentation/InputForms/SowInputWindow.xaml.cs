79f09132a46412ea7686bef86365706068386fc1
Alejoho <alejoreilly96@gmail.com>
2024-05-23 09:33:20 -0400

    Done part of the structure of the code behind of the SowInputWindow

---
+++ Presentation/InputForms/SowInputWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Presentation.IRequesters;
+using System.Windows;
+
+namespace Presentation.InputForms;
+
+//CHECK - What is the property show in the taskbar
+/// <summary>
+/// Interaction logic for SowInputWindow.xaml
+/// </summary>
+public partial class SowInputWindow : Window
+{
+
+    private IOrderLocationChangeRequester _requester;
+    public SowInputWindow(IOrderLocationChangeRequester requestingWindow)
+    {
+        InitializeComponent();
+        _requester = requestingWindow;
+    }
+
+    private void btnConfirm_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void btnCancel_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+}
