820089dab0295c512be2087a94b22ec4a512987f
Alejoho <alejoreilly96@gmail.com>
2024-05-26 18:57:00 -0400

    Added the ZeroToEmptyStringConverter file

---
+++ Presentation/ZeroToEmptyStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Presentation
+{
+    public class ZeroToEmptyStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int intValue && intValue == 0)
+            {
+                return string.Empty;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
