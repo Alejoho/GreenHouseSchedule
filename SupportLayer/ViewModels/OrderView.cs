21dea4bc80425f1a58feaec344f56620558f3e97
Alejoho <alejoreilly96@gmail.com>
2024-05-26 21:07:32 -0400

    Added the partial class OrderView and changed the name of the partial class of order location

---
+++ SupportLayer/ViewModels/OrderView.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupportLayer.Models
+{
+    public partial class Order
+    {
+        [NotMapped]
+        public ObservableCollection<OrderLocation> OrderLocationsView { get; set; }
+    }
+}
