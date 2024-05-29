2833e5a41b51ebaa99530263ed7108e053fa41c3
Alejoho <alejoreilly96@gmail.com>
2024-05-29 11:37:04 -0400

    Added an extension method to get the Current date in the DateOnly type.

---
+++ Presentation/DateOnlyExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Presentation
+{
+    public static class DateOnlyExtensions
+    {
+        public static DateOnly Today(this DateOnly date)
+        {
+            return DateOnly.FromDateTime(DateTime.Today);
+        }
+    }
+}
