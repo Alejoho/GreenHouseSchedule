b21c8c06b6cda35ba8b4bbb46744dc962bd232be
Alejoho <alejoreilly96@gmail.com>
2024-05-29 11:38:13 -0400

    Added another converter to check the state of the order in the dg father.

---
+++ Presentation/OrderLocationsStateToIntConverter.cs
@@ -0,0 +1,39 @@
+﻿using SupportLayer.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace Presentation
+{
+    public class OrderLocationsStateToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ObservableCollection<OrderLocation> orderLocations)
+            {
+                if (orderLocations.Any(x => x.EstimateSowDate < new DateOnly().Today()) &&
+                    orderLocations.Any(x => x.EstimateSowDate == new DateOnly().Today()))
+                {
+                    return 1;
+                }
+                else if (orderLocations.Any(x => x.EstimateSowDate == new DateOnly().Today()))
+                {
+                    return 0;
+                }
+                else if (orderLocations.Any(x => x.EstimateSowDate < new DateOnly().Today()))
+                {
+                    return -1;
+                }
+            }
+
+            throw new FormatException("Error al intentar determinar el color de fondo de una orden.");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
