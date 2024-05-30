b922f2eb6948183846a4200740c630ce965faaba
Alejoho <alejoreilly96@gmail.com>
2024-05-30 07:16:36 -0400

    Implemented the INotifyPropertyChanged in the OrdeeView file

--- SupportLayer/ViewModels/OrderView.cs
+++ SupportLayer/ViewModels/OrderView.cs
@@ -1,11 +1,47 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SupportLayer.Models
 {
-    public partial class Order
+    public partial class Order : INotifyPropertyChanged
     {
         [NotMapped]
-        public ObservableCollection<OrderLocation> OrderLocationsView { get; set; }
+        private int _trigger;
+
+        [NotMapped]
+        public int Trigger
+        {
+            get => _trigger;
+            set
+            {
+                _trigger = value;
+                OnPropertyChanged("OrderLocationsView");
+            }
+        }
+
+        [NotMapped]
+        private ObservableCollection<OrderLocation> _orderLocationView;
+
+        [NotMapped]
+        public ObservableCollection<OrderLocation> OrderLocationsView
+        {
+            get
+            {
+                return _orderLocationView;
+            }
+
+            set
+            {
+                _orderLocationView = value;
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
