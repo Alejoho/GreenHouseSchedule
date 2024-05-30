fea0cb83b0daf0e6150754ce3b878552d5bb40bc
Alejoho <alejoreilly96@gmail.com>
2024-05-30 17:32:15 -0400

    Made great part of the logic of the UnloadWindow

--- Presentation/Forms/UnloadWindow.xaml.cs
+++ Presentation/Forms/UnloadWindow.xaml.cs
@@ -1,27 +1,107 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Domain.Processors;
+using Presentation.IRequesters;
+using SupportLayer.Models;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace Presentation.Forms
-{
+namespace Presentation.Forms;
+
     /// <summary>
     /// Interaction logic for UnloadWindow.xaml
     /// </summary>
-    public partial class UnloadWindow : Window
+public partial class UnloadWindow : Window, IPlacedOrderLocationChangeRequester
     {
+    private ObservableCollection<Order> _orders;
+    private OrderProcessor _orderProcessor;
+    private OrderLocationProcessor _orderLocationProcessor;
+    private OrderLocation _orderLocationInProcess;
+    private DataGrid _activeOrderLocationDataGrid;
+
         public UnloadWindow()
         {
             InitializeComponent();
+        _orderProcessor = new OrderProcessor();
+        _orderLocationProcessor = new OrderLocationProcessor();
+        LoadData();
+    }
+
+    private void LoadData()
+    {
+        _orders = new ObservableCollection<Order>(_orderProcessor.GetNextOrdersToUnload());
+
+        foreach (Order order in _orders)
+        {
+            var orderLocationsToDisplay = order.OrderLocations.Where(x => x.GreenHouseId == 0);
+            order.OrderLocationsView = new ObservableCollection<OrderLocation>(orderLocationsToDisplay);
+        }
+
+        dgSowList.DataContext = this;
+        dgSowList.ItemsSource = _orders;
+    }
+
+    private void btnCancel_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+
+    private void btnUnload_Click(object sender, RoutedEventArgs e)
+    {
+        CallOrderLocationPlaceSetter();
+    }
+
+    private void DataGridRow_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        CallOrderLocationPlaceSetter();
+    }
+
+    private void CallOrderLocationPlaceSetter()
+    {
+        }
+
+    private void btnRowDetail_Click(object sender, RoutedEventArgs e)
+    {
+        var row = DataGridRow.GetRowContainingElement((Button)sender);
+
+        row.DetailsVisibility = row.DetailsVisibility == Visibility.Visible ?
+        Visibility.Collapsed : Visibility.Visible;
+    }
+
+    private void dgOrderLocationChild_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is DataGrid datagrid)
+        {
+            _orderLocationInProcess = (OrderLocation)datagrid.SelectedItem;
+            _activeOrderLocationDataGrid = datagrid;
         }
     }
+
+    public void SetThePlacedOrderLocation(int greenHouse, int block, short sownSeedTrays)
+    {
+
+        _orderLocationProcessor.SavePlacedOrderLocationChange(_orderLocationInProcess, greenHouse, block, sownSeedTrays);
+
+        RefreshTheDataGrids();
+    }
+
+    private void RefreshTheDataGrids()
+    {
+        if (_orderLocationInProcess.Order.Complete == true)
+        {
+            _orders.Remove(_orderLocationInProcess.Order);
+        }
+        else if (_orderLocationInProcess.RealSowDate != null)
+        {
+            Order order = _orderLocationInProcess.Order;
+            order.OrderLocationsView.Remove(_orderLocationInProcess);
+            order.UIColorsUpdateTrigger++;
+        }
+        else
+        {
+            _activeOrderLocationDataGrid.Items.Refresh();
+        }
+    }
+
+    public OrderLocation OrderLocationInProcess { get => _orderLocationInProcess; }
 }
