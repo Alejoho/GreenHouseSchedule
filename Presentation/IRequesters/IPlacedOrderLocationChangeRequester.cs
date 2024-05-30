eceee4e25339f5fb2512b7102f8a193f79e74464
Alejoho <alejoreilly96@gmail.com>
2024-05-30 17:29:24 -0400

    Added a new IRequester

---
+++ Presentation/IRequesters/IPlacedOrderLocationChangeRequester.cs
@@ -0,0 +1,10 @@
+﻿using SupportLayer.Models;
+
+namespace Presentation.IRequesters
+{
+    public interface IPlacedOrderLocationChangeRequester
+    {
+        void SetThePlacedOrderLocation(int greenHouse, int block, short sownSeedTrays);
+        OrderLocation OrderLocationInProcess { get; }
+    }
+}
