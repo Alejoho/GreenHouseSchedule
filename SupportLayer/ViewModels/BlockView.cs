20016b57acde02c017accd56686cdf66d5394378
Alejoho <alejoreilly96@gmail.com>
2024-06-04 11:58:42 -0400

    Created the BlockView file

---
+++ SupportLayer/ViewModels/BlockView.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupportLayer.Models
+{
+    public partial class Block
+    {
+        [NotMapped]
+        public int SeedlingAmount { get; set; }
+
+        public string BlockName
+        {
+            get
+            {
+                return "Bloque " + BlockNumber;
+            }
+        }
+    }
+}
