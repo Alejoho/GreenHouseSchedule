3688824b8d5f69fcf3adcc7b04b4bb93de98acd5
Alejoho <alejoreilly96@gmail.com>
2024-06-07 10:49:44 -0400

    Change the name and some of the logic of some properties of the BlockView file. And implemented those changes in the DeliveryWindow.

--- SupportLayer/ViewModels/BlockView.cs
+++ SupportLayer/ViewModels/BlockView.cs
@@ -5,12 +5,23 @@ namespace SupportLayer.Models
     public partial class Block
     {
         [NotMapped]
-        public int SeedlingAmount
+        public int SeedTraysAmountToBeDelivered
+        {
+            get
+            {
+                int seedTraysAlreadyDelivered = DeliveryDetails.Sum(x => x.SeedTrayAmountDelivered);
+                int seedTraysToBeDelivered = SeedTrayAmount - seedTraysAlreadyDelivered;
+                return seedTraysToBeDelivered;
+            }
+        }
+
+        [NotMapped]
+        public int SeedlingAmountToBeDelivered
         {
             get
             {
                 int alveolus = OrderLocation.SeedlingAmount / OrderLocation.SeedTrayAmount;
-                return alveolus * SeedTrayAmount;
+                return alveolus * SeedTraysAmountToBeDelivered;
             }
         }
 
