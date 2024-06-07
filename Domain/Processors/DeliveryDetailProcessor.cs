e5233f73903c0d5ae22d2098a2d222861ca15b7f
Alejoho <alejoreilly96@gmail.com>
2024-06-07 08:35:20 -0400

    Made the logic to save the delivered blocks in the DeliveryDetailProcessor.

--- Domain/Processors/DeliveryDetailProcessor.cs
+++ Domain/Processors/DeliveryDetailProcessor.cs
@@ -19,4 +19,34 @@ public class DeliveryDetailProcessor : IDeliveryDetailProcessor
 
         return output;
     }
+
+    public void SaveNewDeliveryDetails(Block block, DateOnly date, short deliveredSeedTrays)
+    {
+        ValidateDeliveryChanges(block, date, deliveredSeedTrays);
+
+        DeliveryDetail deliveryDetail = new DeliveryDetail()
+        {
+            BlockId = block.Id,
+            DeliveryDate = date,
+            SeedTrayAmountDelivered = deliveredSeedTrays
+        };
+
+        _repository.Insert(deliveryDetail);
+
+        block.SeedTrayAmount -= deliveredSeedTrays;
+    }
+
+    private void ValidateDeliveryChanges(Block block, DateOnly date, short deliveredSeedTrays)
+    {
+        if (date > DateOnly.FromDateTime(DateTime.Now))
+        {
+            throw new ArgumentException("La fecha debe ser igual o anterior que el dia presente", "date");
+        }
+
+        if (deliveredSeedTrays <= 0 || deliveredSeedTrays > block.SeedTrayAmount)
+        {
+            throw new ArgumentException("La cantidad de bandejas entregadas debe estar entre 0 " +
+                "y la cantidad de bandejas del bloque", "deliveredSeedTrays");
+        }
+    }
 }
