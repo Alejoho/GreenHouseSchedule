4fd22c2bb1970125af9e5299bd76504a6e12d87f
Alejoho <alejoreilly96@gmail.com>
2024-06-11 08:47:45 -0400

    Created and completed the RelocateInputWindow

---
+++ Presentation/InputForms/RelocateInputWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using Domain.Processors;
+using Presentation.IRequesters;
+using SupportLayer.Models;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Presentation.InputForms
+{
+    /// <summary>
+    /// Interaction logic for RelocateInputWindow.xaml
+    /// </summary>
+    public partial class RelocateInputWindow : Window
+    {
+        private IRelocatedBlockRequester _requester;
+
+        public RelocateInputWindow(IRelocatedBlockRequester requestingWindow)
+        {
+            InitializeComponent();
+            _requester = requestingWindow;
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            GreenHouseProcessor processor = new GreenHouseProcessor();
+            var greenHouses = processor.GetActiveGreenHouses();
+
+            lblcmbGreenHouse.ComboBox.ItemsSource = greenHouses;
+            lblcmbGreenHouse.ComboBox.DisplayMemberPath = "Name";
+        }
+
+        private void lblcmbGreenHouse_Selection_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            int amountOfBlocks = ((GreenHouse)lblcmbGreenHouse.ComboBox.SelectedItem).AmountOfBlocks;
+
+            lblcmbBlock.ComboBox.Items.Clear();
+
+            for (int i = 1; i <= amountOfBlocks; i++)
+            {
+                lblcmbBlock.ComboBox.Items.Add(i);
+            }
+        }
+
+        private void btnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateData() == true)
+            {
+                try
+                {
+                    byte greenHouseId = ((GreenHouse)lblcmbGreenHouse.ComboBox.SelectedItem).Id;
+                    byte blockNumber = Convert.ToByte(lblcmbBlock.ComboBox.SelectedItem);
+                    short placedSeedTrays = short.Parse(lbltxtPlacedAmount.FieldContent);
+
+                    _requester.SetTheRelocatedBlock(greenHouseId, blockNumber, placedSeedTrays);
+                    this.Close();
+                }
+                catch (ArgumentException ex)
+                {
+                    int endIndex = ex.Message.IndexOf('(');
+
+                    endIndex--;
+
+                    MessageBox.Show($"{ex.Message.Substring(0, endIndex)}.");
+
+                    if (ex.ParamName == "something")
+                    {
+                        lbltxtPlacedAmount.TextBox.Focus();
+                    }
+                }
+                //catch (Exception ex)
+                //{
+                //    //LATER - Implement the log.
+                //    MessageBox.Show($"{ex.Message}");
+                //}
+            }
+        }
+
+        private bool ValidateData()
+        {
+            if (lblcmbGreenHouse.ComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Debe especificar la casa en la que se reubicaron estas bandejas"
+                    , "Dato faltante"
+                    , MessageBoxButton.OK, MessageBoxImage.Information);
+                lblcmbGreenHouse.ComboBox.Focus();
+                return false;
+            }
+
+            if (lblcmbBlock.ComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Debe especificar el bloque en el que se reubicaron estas bandejas"
+                    , "Dato faltante"
+                    , MessageBoxButton.OK, MessageBoxImage.Information);
+                lblcmbBlock.ComboBox.Focus();
+                return false;
+            }
+
+            if (lbltxtPlacedAmount.FieldContent == null
+                || lbltxtPlacedAmount.FieldContent == "")
+            {
+                MessageBox.Show("Debe especificar la cantidad de bandejas reubicadas.", "Dato faltante"
+                    , MessageBoxButton.OK, MessageBoxImage.Information);
+                lbltxtPlacedAmount.TextBox.Focus();
+                return false;
+            }
+            else if (short.TryParse(lbltxtPlacedAmount.FieldContent, out short amountOfSeedlings) == false)
+            {
+                MessageBox.Show("La cantidad de bandejas reubicadas no esta en el formato correcto."
+                    , "Cantidad de bandejas ubicadas inválida"
+                    , MessageBoxButton.OK, MessageBoxImage.Warning);
+                lbltxtPlacedAmount.TextBox.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
