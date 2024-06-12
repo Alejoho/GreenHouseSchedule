67678992785bead01756763fae6e66f15751ab8b
Alejoho <alejoreilly96@gmail.com>
2024-06-12 15:03:55 -0400

    Transfer 2 methods from the OrderLocationProcessor

--- Domain/Processors/BlockProcessor.cs
+++ Domain/Processors/BlockProcessor.cs
@@ -92,6 +92,43 @@ namespace Domain.Processors
                 orderLocationRepository.Update(orderLocationOriginal);
             }
         }
+        private OrderLocation GetCopyOfAnOrderLocation(OrderLocation orderLocation)
+        {
+            return new OrderLocation()
+            {
+                Id = 0,
+                GreenHouseId = orderLocation.GreenHouseId,
+                SeedTrayId = orderLocation.SeedTrayId,
+                OrderId = orderLocation.OrderId,
+                SeedTrayAmount = orderLocation.SeedTrayAmount,
+                SeedlingAmount = orderLocation.SeedlingAmount,
+                EstimateSowDate = orderLocation.EstimateSowDate,
+                EstimateDeliveryDate = orderLocation.EstimateDeliveryDate,
+                RealSowDate = orderLocation.RealSowDate,
+                RealDeliveryDate = orderLocation.RealDeliveryDate
+            };
+        }
+
+        private OrderLocation GetOrderLocationsBrother(OrderLocation orderLocation, byte greenHouse)
+        {
+            Order order = orderLocation.Order;
+
+            var output = order.OrderLocations.Where(x =>
+                    x.GreenHouseId == greenHouse
+                    && x.SeedTrayId == orderLocation.SeedTrayId
+                    && x.RealSowDate == orderLocation.RealSowDate);
+
+            if (output.Count() > 1)
+            {
+                throw new ApplicationException("There's more than 1 order location brother.");
+            }
+
+            if (output.Count() == 0)
+            {
+                throw new ApplicationException("There's no order location brother.");
+            }
+
+            return output.First();
         }
 
         public void SaveRelocateBlockChange(Block blockInProcess, byte greenHouse, byte block, short relocatedSeedTrays)
