569405b2b5af8774ab14aeeaa99c77b9c1460cdb
Alejoho <alejoreilly96@gmail.com>
2024-06-17 16:29:20 -0400

    Ready the filter logic in the OrderListWindow.

--- Presentation/Forms/OrderListWindow.xaml.cs
+++ Presentation/Forms/OrderListWindow.xaml.cs
@@ -13,9 +13,6 @@ namespace Presentation.Forms;
 public partial class OrderListWindow : Window
 {
     //LATER - Add HeadersVisibility = "Column" to the datagrids across the board
-    //LATER - Find out how to delete and order. Now I couldn't because the relation between order and orderlocations
-    //is severed so i have to delete the orderlocations first in order to delete the actual order. I think I could set
-    //"delete on cascade".
     private OrderProcessor _processor;
     private Orders _orders;
     private CollectionViewSource _viewSource;
@@ -72,19 +69,53 @@ public partial class OrderListWindow : Window
 
     private void lbltxtSearch_TextChanged(object sender, TextChangedEventArgs e)
     {
-        //LATER - Change the implementation of all SearchTextBoxes.
-        //Every time the text change a call to the database is made that is not performant
-        //I'd rather to retrieve all records from the db and  then filter in the processor.
+        _viewSource.View.Refresh();
+    }
+
     private void CollectionViewSource_Filter(object sender, System.Windows.Data.FilterEventArgs e)
     {
-        string filter = lbltxtSearch.TextBox.Text;
-        _orders = new ObservableCollection<Order>(_processor.GetFilteredOrders(filter));
-        dgOrderList.ItemsSource = null;
-        dgOrderList.ItemsSource = _orders;
+        Order order = e.Item as Order;
+
+        if (order != null)
+        {
+            e.Accepted = false;
+
+            string filter = lbltxtSearch.TextBox.Text;
+            string dateFormat = (string)Application.Current.Resources["DateFormat"];
+
+            if (order.Id.ToString().Contains(filter, System.StringComparison.CurrentCultureIgnoreCase)
+                || order.Client.Name.Contains(filter, System.StringComparison.CurrentCultureIgnoreCase)
+                || order.Product.Specie.Name.Contains(filter, System.StringComparison.CurrentCultureIgnoreCase)
+                || order.Product.Variety.Contains(filter, System.StringComparison.CurrentCultureIgnoreCase)
+                || order.AmountOfWishedSeedlings.ToString().Contains(filter, System.StringComparison.CurrentCultureIgnoreCase)
+                || order.AmountOfAlgorithmSeedlings.ToString().Contains(filter, System.StringComparison.CurrentCultureIgnoreCase)
+                || order.DateOfRequest.ToString(dateFormat).Contains(filter, System.StringComparison.CurrentCultureIgnoreCase)
+                || order.WishDate.ToString(dateFormat).Contains(filter, System.StringComparison.CurrentCultureIgnoreCase)
+                || order.EstimateSowDate.ToString(dateFormat).Contains(filter, System.StringComparison.CurrentCultureIgnoreCase)
+                || order.EstimateDeliveryDate.ToString(dateFormat).Contains(filter, System.StringComparison.CurrentCultureIgnoreCase))
+            {
+                e.Accepted = true;
+            }
+
+            if (order.RealSowDate.HasValue
+                && order.RealSowDate.Value.ToString(dateFormat)
+                .Contains(filter, System.StringComparison.CurrentCultureIgnoreCase))
+            {
+                e.Accepted = true;
+            }
+
+            if (order.RealDeliveryDate.HasValue
+                && order.RealDeliveryDate.Value.ToString(dateFormat)
+                .Contains(filter, System.StringComparison.CurrentCultureIgnoreCase))
+            {
+                e.Accepted = true;
+            }
+        }
     }
 }
 
 //LATER - Give to this class a better name. And extract it in a separated file
+// or maybe remove it and do these things the way they should be done
 public class DataContextHolderClass
 {
     private ObservableCollection<Order> _orders;
