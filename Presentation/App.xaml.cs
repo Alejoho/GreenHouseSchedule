98e3ce29f7945e7fae9dfe2550f452cf160cf6f6
Alejoho <alejoreilly96@gmail.com>
2024-06-22 16:05:27 -0400

    Added the log4net reference and a helper class to get the loggers.

--- Presentation/App.xaml.cs
+++ Presentation/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
 namespace Presentation
 {
     /// <summary>
@@ -7,5 +9,7 @@ namespace Presentation
     /// </summary>
     public partial class App : Application
     {
+
+        //NEXT - Ver cual es el evento que se dispara al abrir y cerrar la app y poner un log en los dos.
     }
 }
---
+++ SupportLayer/LogHelper.cs
@@ -0,0 +1,13 @@
+﻿using log4net;
+using System.Runtime.CompilerServices;
+
+namespace SupportLayer
+{
+    public static class LogHelper
+    {
+        public static ILog GetLogger([CallerFilePath] string filePath = "")
+        {
+            return LogManager.GetLogger(filePath);
+        }
+    }
+}
