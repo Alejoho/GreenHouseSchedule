f340f6200c94a14bb55f4f8e6e657b049740d671
Alejoho <alejoreilly96@gmail.com>
2024-06-23 11:01:47 -0400

    Log the start and close of the app

--- Presentation/App.xaml.cs
+++ Presentation/App.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System.Windows;
-
-[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+﻿using log4net;
+using SupportLayer;
+using System.Windows;
 
 namespace Presentation
 {
@@ -9,6 +9,16 @@ namespace Presentation
     /// </summary>
     public partial class App : Application
     {
+        private static readonly ILog _log = LogHelper.GetLogger();
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            _log.Info("The app was launched");
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            _log.Info("The app was exited");
+        }
 
         //NEXT - Ver cual es el evento que se dispara al abrir y cerrar la app y poner un log en los dos.
     }
