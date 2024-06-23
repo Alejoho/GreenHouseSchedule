b0561cb53243f4ddbb7fbee59161c63a8fef87e5
Alejoho <alejoreilly96@gmail.com>
2024-06-23 11:51:02 -0400

    Added the property formatter to the support layer

---
+++ SupportLayer/PropertyFormatter.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace SupportLayer;
+
+public class PropertyFormatter
+{
+    public static string FormatProperties(object obj)
+    {
+        if (obj == null)
+        {
+            throw new ArgumentNullException(nameof(obj));
+        }
+
+        var properties = obj.GetType().GetProperties();
+        StringBuilder builder = new StringBuilder();
+
+        foreach (var property in properties)
+        {
+            builder.Append($"{property.Name} = {property.GetValue(obj)}, ");
+        }
+
+        if (builder.Length > 2)
+        {
+            builder.Length -= 2;
+        }
+
+        return builder.ToString();
+    }
+}
