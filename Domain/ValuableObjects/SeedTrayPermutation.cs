3398813dd50315fa64648f9b3a93162ec5be262a
Alejoho <alejoreilly96@gmail.com>
2024-06-30 10:52:59 -0400

    Made the documentation of this class.

--- Domain/ValuableObjects/SeedTrayPermutation.cs
+++ Domain/ValuableObjects/SeedTrayPermutation.cs
@@ -1,6 +1,8 @@
 ﻿namespace Domain.ValuableObjects;
 
-//LATER - Document this class
+/// <summary>
+/// This class represent the different permutations the seedtrays could make until 3 level items.
+/// </summary>
 public class SeedTrayPermutation
 {
     private int _firstSeedTrayID;
@@ -13,6 +15,18 @@ public class SeedTrayPermutation
     private string _thirdSeedTrayName;
     private int _thirdAmount;
 
+    /// <summary>
+    /// Creates a new instance with only the items passed as arguments.
+    /// </summary>
+    /// <param name="pFirstSeedTrayID">Id of the first <c>SeedTray</c> type.</param>
+    /// <param name="pfirstSeedTrayName">Name of the first <c>SeedTray</c> type.</param>
+    /// <param name="pFirstAmount">Amount of the first <c>SeedTray</c> type.</param>
+    /// <param name="pSecondSeedTrayID2">Id of the second <c>SeedTray</c> type.</param>
+    /// <param name="pSecondSeedTrayName">Name of the second <c>SeedTray</c> type.</param>
+    /// <param name="pSecondAmount">Amount of the second <c>SeedTray</c> type.</param>
+    /// <param name="pThirdSeedTrayID3">Id of the third <c>SeedTray</c> type.</param>
+    /// <param name="pThirdSeedTrayName">Name of the third <c>SeedTray</c> type.</param>
+    /// <param name="pThirdAmount">Amount of the third <c>SeedTray</c> type.</param>
     public SeedTrayPermutation(int pFirstSeedTrayID, string pfirstSeedTrayName, int pFirstAmount
         , int pSecondSeedTrayID2 = 0, string pSecondSeedTrayName = "", int pSecondAmount = 0
         , int pThirdSeedTrayID3 = 0, string pThirdSeedTrayName = "", int pThirdAmount = 0)
@@ -28,6 +42,10 @@ public class SeedTrayPermutation
         _thirdAmount = pThirdAmount;
     }
 
+    /// <summary>
+    /// Creates a new copy instance of another <c>SeedTray</c> object.
+    /// </summary>
+    /// <param name="pSeedTrayPermutationOriginal">The original <c>SeedTray</c> object.</param>
     public SeedTrayPermutation(SeedTrayPermutation pSeedTrayPermutationOriginal)
     {
         this._firstSeedTrayID = pSeedTrayPermutationOriginal.FirstSeedTrayID;
@@ -41,21 +59,48 @@ public class SeedTrayPermutation
         this._thirdAmount = pSeedTrayPermutationOriginal.ThirdAmount;
     }
 
+    /// <summary>
+    /// Gets or sets the Id of the first <c>SeedTray</c> type.
+    /// </summary>
     public int FirstSeedTrayID { get => _firstSeedTrayID; set => _firstSeedTrayID = value; }
 
+    /// <summary>
+    /// Gets or sets the Name of the first <c>SeedTray</c> type.
+    /// </summary>
     public string FirstSeedTrayName { get => _firstSeedTrayName; set => _firstSeedTrayName = value; }
 
+    /// <summary>
+    /// Gets or sets the Amount of the first <c>SeedTray</c> type.
+    /// </summary>
     public int FirstAmount { get => _firstAmount; set => _firstAmount = value; }
 
+    /// <summary>
+    /// Gets or sets the Id of the second <c>SeedTray</c> type.
+    /// </summary>
     public int SecondSeedTrayID { get => _secondSeedTrayID; set => _secondSeedTrayID = value; }
 
+    /// <summary>
+    /// Gets or sets the Name of the second <c>SeedTray</c> type.
+    /// </summary>
     public string SecondSeedTrayName { get => _secondSeedTrayName; set => _secondSeedTrayName = value; }
 
+    /// <summary>
+    /// Gets or sets the Amount of the second <c>SeedTray</c> type.
+    /// </summary>
     public int SecondAmount { get => _secondAmount; set => _secondAmount = value; }
 
+    /// <summary>
+    /// Gets or sets the Id of the third <c>SeedTray</c> type.
+    /// </summary>
     public int ThirdSeedTrayID { get => _thirdSeedTrayID; set => _thirdSeedTrayID = value; }
 
+    /// <summary>
+    /// Gets or sets the Name of the third <c>SeedTray</c> type.
+    /// </summary>
     public string ThirdSeedTrayName { get => _thirdSeedTrayName; set => _thirdSeedTrayName = value; }
 
+    /// <summary>
+    /// Gets or sets the Amount of the third <c>SeedTray</c> type.
+    /// </summary>
     public int ThirdAmount { get => _thirdAmount; set => _thirdAmount = value; }
 }
