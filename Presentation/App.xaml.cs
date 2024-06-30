f8a239571253ec8273413b60f75144c2080a70c8
Alejoho <alejoreilly96@gmail.com>
2024-06-30 17:52:46 -0400

    Added the 2 global error handlers

--- Presentation/App.xaml.cs
+++ Presentation/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using log4net;
 using SupportLayer;
+using System;
 using System.Windows;
 
 namespace Presentation
@@ -11,6 +12,14 @@ namespace Presentation
     public partial class App : Application
     {
         private static readonly ILog _log = LogHelper.GetLogger();
+        private bool _wasTheMessageDisplayed;
+
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            _wasTheMessageDisplayed = false;
+        }
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             _log.Info("The app was launched");
@@ -22,6 +31,42 @@ namespace Presentation
             _log.Info("____________________________________________________________" +
                 "____________________________________________________________________" +
                 "________________________");
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+
+            var ex = e.Exception;
+            _log.Fatal("It was an unexpected error catch by the DispatcherUnhandledException and the app crashed", ex);
+            _log.Fatal("____________________________________________________________" +
+                "____________________________________________________________________" +
+                "________________________");
+
+            if (_wasTheMessageDisplayed == false)
+            {
+                MessageBox.Show("Hubo un error inesperado. La applicación va a cerrarse."
+                    , "Fatal Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                _wasTheMessageDisplayed = true;
+            }
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = (Exception)e.ExceptionObject;
+
+            _log.Fatal("It was an unexpected error catch by the UnhandledException and the app crashed", ex);
+            _log.Fatal("____________________________________________________________" +
+                "____________________________________________________________________" +
+                "________________________");
+
+            if (_wasTheMessageDisplayed == false)
+            {
+                MessageBox.Show("Hubo un error inesperado. La applicación va a cerrarse."
+                    , "Fatal Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                _wasTheMessageDisplayed = true;
+            }
         }
     }
 }
