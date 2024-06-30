51ae43cbd94c43ffb566b565b3585cf829b6a9d2
Alejoho <alejoreilly96@gmail.com>
2024-06-30 12:24:43 -0400

    Removed unused code in the Species repo and processor

--- DataAccess/Repositories/SpeciesRepository.cs
+++ DataAccess/Repositories/SpeciesRepository.cs
@@ -18,11 +18,6 @@ namespace DataAccess.Repositories
         {
             return _sowScheduleDB.Species;
         }
-        //LATER - Make a test for this method
-        public Species GetByID(int pId)
-        {
-            return _sowScheduleDB.Species.Find((byte)pId);
-        }
 
         public bool Insert(Species entity)
         {
